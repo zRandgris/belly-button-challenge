bbb3cd0e916edcc82e51c57b4acd78531c9ab0b5
KevinW <kevinwan830803@gmail.com>
2023-08-08 18:36:19 -0400

    swap the url link from personal back to the original

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 //Get the data endpoint store as url
-//const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-const url = "./samples.json"
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+//const url = "./samples.json"
 
 // Fetch the JSON data and console log it
 d3.json(url).then((data)=> {
